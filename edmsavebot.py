080c56a4cd456265537038c4416630e9e748e31c
link2x <link2x101@gmail.com>
2015-09-16 06:10:20 -0400

    Revert "Version 1.1.1"
    This reverts commit 065db54cd13b80cba66657202f0c104edcf0e949.

--- edmsavebot.py
+++ edmsavebot.py
@@ -1,16 +1,13 @@
 # EDMSaveBot
 # By: /u/link2x (http://link2x.us/)
 #
-# Version 1.1.2
+# Version 1.1.1
 #
 # Purpose:
 #   This bot is intended to save the original contents of posts linked to by /r/EDMProdCircleJerk.
 #
 #
 # CHANGES:
-#
-# Version 1.1.2
-#   - Subreddit can now be passed from command-line.
 #
 # Version 1.1.1
 #   - Login information can now be passed from command-line.
@@ -40,27 +37,21 @@ print("D: PRAW initialized")
 commandParse = argparse.ArgumentParser()
 commandParse.add_argument("-username",help="reddit username",type=str)
 commandParse.add_argument("-password",help="reddit password",type=str)
-commandParse.add_argument("-subreddit",help="subreddit to run on",type=str)
 commandInput = commandParse.parse_args()
 
 redditUser = None
 redditUser = commandInput.username
 redditPass = None
 redditPass = commandInput.password
-redditSub = None
-redditSub = commandInput.subreddit
 
 if (not redditUser):
     redditUser = input("Bot account name: ")
 if (not redditPass):
     redditPass = input("Bot account pass: ")
-if (not redditSub):
-    redditSub = input("Subreddit to run on: ")
 
 r.login(redditUser,redditPass) # Connect to reddit
 
 print("D: Logged in to reddit")
-print("D: "+redditUser+" SUB: "+redditSub)
 
 already_done = [] # Used to help avoid re-commenting, doesn't last through reboots currently.
 regexString = '/(.{7})(/*)$|\n' # This is used to differentiate between links to posts and links to comments
@@ -68,7 +59,7 @@ regexRE = re.compile(regexString) # This allows the above string to be used to s
 
 while True: #Main loop
     print("D: Loop begin")
-    subreddit = r.get_subreddit(redditSub) #Set our subreddit. Glorious master race
+    subreddit = r.get_subreddit('EDMSaveBotDev') #Set our subreddit. Glorious master race
     for submission in subreddit.get_new(limit=10): #We'll look at the 10 newest posts
         if ((submission.is_self==False) and (submission.domain=='reddit.com')): #If the post is a link pointing at reddit
             if any(string in submission.url for string in '/comments/'): #Verify it's a post or comment we're being linked to
